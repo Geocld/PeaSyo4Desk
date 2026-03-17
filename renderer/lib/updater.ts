20a8ad527ae416e8e7801353bcddd92538495cfe
lijiahao <lijiahao@cmschina.com.cn>
2026-03-17 15:59:16 +0800

    update

--- renderer/lib/updater.ts
+++ renderer/lib/updater.ts
@@ -1,44 +1,44 @@
-import axios from 'axios';
-import semver from 'semver';
-import pkg from '../../package.json';
+// import axios from 'axios';
+// import semver from 'semver';
+// import pkg from '../../package.json';
 
-const CHECK_URL = 'https://api.github.com/repos/Geocld/PeaSyo4Desk/releases';
+// const CHECK_URL = 'https://api.github.com/repos/Geocld/PeaSyo4Desk/releases';
 
 const updater = () => {
-  const {version} = pkg;
+  // const {version} = pkg;
   return new Promise(resolve => {
     resolve(false); // Disable update check for now
-    return;
-    axios
-      .get(CHECK_URL, {timeout: 30 * 1000})
-      .then(res => {
-        if (res.status === 200) {
-          const releases = res.data;
-          console.log('releases:', releases)
-          if (releases.length > 0) {
-            const latest = releases[0];
-            const latestVer = semver.valid(semver.coerce(latest.tag_name));
-            if (latestVer && semver.gt(latestVer, version)) {
-              // Have new version
-              resolve({
-                latestVer,
-                version,
-                url: latest.html_url,
-              });
-            } else {
-              resolve(false);
-            }
-          } else {
-            resolve(false);
-          }
-        } else {
-          resolve(false);
-        }
-      })
-      .catch(e => {
-        console.log('Check version error:', e);
-        resolve(false);
-      });
+    // return;
+    // axios
+    //   .get(CHECK_URL, {timeout: 30 * 1000})
+    //   .then(res => {
+    //     if (res.status === 200) {
+    //       const releases = res.data;
+    //       console.log('releases:', releases)
+    //       if (releases.length > 0) {
+    //         const latest = releases[0];
+    //         const latestVer = semver.valid(semver.coerce(latest.tag_name));
+    //         if (latestVer && semver.gt(latestVer, version)) {
+    //           // Have new version
+    //           resolve({
+    //             latestVer,
+    //             version,
+    //             url: latest.html_url,
+    //           });
+    //         } else {
+    //           resolve(false);
+    //         }
+    //       } else {
+    //         resolve(false);
+    //       }
+    //     } else {
+    //       resolve(false);
+    //     }
+    //   })
+    //   .catch(e => {
+    //     console.log('Check version error:', e);
+    //     resolve(false);
+    //   });
   });
 };
 
